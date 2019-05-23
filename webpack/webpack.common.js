10da0e15a16e04cae58201cec5f06303c1a30a0a
Scott Yano <stmhawaii@gmail.com>
2019-05-23 18:54:15 +0900

    Create a way to easily add example pages

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,24 @@ const Path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+
+
+var examplePages = [];
+fs.readdirSync(Path.resolve(__dirname, '../examples/')).forEach((file) => {
+  var fileName = file.replace(/^.*[\\\/]/, '');
+  if (!fileName.endsWith('html')) {
+    return;
+  }
+
+  var htmlPage = new HtmlWebpackPlugin({
+    filename: fileName,
+    template: Path.resolve(__dirname, `../examples/${fileName}`)
+  });
+  examplePages.push(htmlPage);
+});
+
 
 module.exports = {
   entry: {
@@ -22,10 +40,12 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: Path.resolve(__dirname, '../examples/public'), to: 'public' }
     ]),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../examples/index.html')
-    })
-  ],
+    // new HtmlWebpackPlugin({
+    //   template: Path.resolve(__dirname, '../examples/index.html')
+    // })
+  ].concat(
+    examplePages
+  ),
   resolve: {
     alias: {
       '~': Path.resolve(__dirname, '../examples')
