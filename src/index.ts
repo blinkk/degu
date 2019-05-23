76e8f9f106f08fcd2684e84bf23e7ef849a2e721
Scott Yano <stmhawaii@gmail.com>
2019-05-23 17:25:37 +0900

    Add a root yano object.

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { array } from './array';
+import { asserts } from './asserts';
+import { AutoEaser } from './autoeaser';
+import { colors } from './colors';
+import { ConvexHull } from './convexhull';
+import { mathf } from './mathf';
+import { MouseTracker } from './mousetracker';
+import { Parallaxer } from './parallaxer';
+import { Raf } from './raf';
+import { Vector2 } from './vector2';
+
+export {
+    array,
+    asserts,
+    AutoEaser,
+    colors,
+    ConvexHull,
+    mathf,
+    MouseTracker,
+    Parallaxer,
+    Raf,
+    Vector2
+};
+
