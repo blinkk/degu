1de0a0912f701781b5730a2821964c395706b77e
Scott Yano <stmhawaii@gmail.com>
2019-05-25 07:40:20 +0900

    Remove classes that aren’t ready from release

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-import { array } from './array';
-import { asserts } from './asserts';
-import { AutoEaser } from './autoeaser';
-import { colors } from './colors';
-import { ConvexHull } from './convexhull';
+// import { array } from './array';
+// import { asserts } from './asserts';
+// import { AutoEaser } from './autoeaser';
+// import { colors } from './colors';
+// import { ConvexHull } from './convexhull';
 import { dom } from './dom';
 import { mathf } from './mathf';
-import { MouseTracker } from './mousetracker';
-import { Parallaxer } from './parallaxer';
+// import { MouseTracker } from './mousetracker';
+// import { Parallaxer } from './parallaxer';
 import { Raf } from './raf';
-import { Vector2 } from './vector2';
 
 export {
-    array,
-    asserts,
-    AutoEaser,
-    colors,
-    ConvexHull,
     dom,
     mathf,
-    MouseTracker,
-    Parallaxer,
     Raf,
-    Vector2
 };
