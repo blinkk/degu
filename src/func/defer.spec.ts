686340d52169443bd630632ea12f115962495482
Scott Yano <stmhawaii@gmail.com>
2019-06-03 11:54:15 +0900

    Add basic defer class

---
+++ src/func/defer.spec.ts
@@ -0,0 +1,23 @@
+import { Defer } from './defer';
+import test from 'ava';
+import { deepStrictEqual } from 'assert';
+
+
+
+test('defer', async t => {
+
+    let defer = new Defer();
+
+    let count = 0;
+    let resolveLater = () => {
+        window.setTimeout(() => {
+            count++;
+            defer.resolve();
+        })
+    }
+    t.is(count, 0);
+    resolveLater();
+    await defer.getPromise();
+    t.is(count, 1);
+
+});
--- src/func/defer.ts
+++ src/func/defer.ts
@@ -1,5 +1,58 @@
+import { promises } from "fs";
+
 //https://gist.github.com/uxder/b9bd3811e0508fae42c1df64155b9c8b
 
+/**
+ * A basic deferred promise.  Allows you to resolve/reject a promise at a later
+ * time.
+ *
+ *
+ * ```ts
+ *
+ * let defer = new Defer();
+ *
+ * // Resolve the defer at a later time.
+ * let hello = ()=> {
+ *   defer.resolve({ message : 'you said hello'});
+ *
+ *   // If you need to reject.
+ *   // defer.reject();
+ * }
+ *
+ *
+ * // Get the promise and handle it.
+ * defer.getPromise().then((data)=> {
+ *   console.log(data.message);
+ * })
+ *
+ * window.setTimeout(()=> {
+ *   hello():
+ * }, 200)
+ *
+ *
+ *
+ * ```
+ *
+ * @tested
+ */
 export class Defer {
+    public resolve: Function;
+    public promise: Promise<any>;
+    public reject: Function;
+    constructor() {
+
+        this.resolve = () => { };
+        this.reject = () => { };
+        this.promise = new Promise((resolve: Function, reject: Function) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+
+        Object.freeze(this);
+    }
+
+    getPromise(): Promise<any> {
+        return this.promise;
+    }
 
 }
