f2ba4f93c869b01c76d77ca8e7230ee0aae4e247
Scott Yano <stmhawaii@gmail.com>
2019-06-08 06:03:36 +0900

    Update matfh.lerp

--- src/mathf/mathf.ts
+++ src/mathf/mathf.ts
@@ -800,9 +800,9 @@ export class mathf {
    * @return {number} The interporalated value.
    */
   static lerp(value1: number, value2: number, amount: number): number {
-    amount = amount < 0 ? 0 : amount;
-    amount = amount > 1 ? 1 : amount;
-    return value1 + (value2 - value1) * amount;
+    // amount = amount < 0 ? 0 : amount;
+    // amount = amount > 1 ? 1 : amount;
+    return value1 + (value2 - value1) * mathf.clamp01(amount);
 
 
     // Alternative ways to calculate lerp
