f4f67e4d0381cde6235f1046ef299a104e65e5dc
Scott Yano <stmhawaii@gmail.com>
2019-06-19 17:10:47 +0900

    Update perlin noise sample

--- examples/perlin-noise.js
+++ examples/perlin-noise.js
@@ -19,25 +19,38 @@ export default class PerlinNoiseSample {
         this.draw();
     }
 
+    // Draws one line.
+    oneline(amplitude) {
+        this.context.beginPath();
+        for (let i = 0; i < canvas.width; i += 30) {
+            let x = i / canvas.width;
+            let y = this.y / canvas.height;
+            let z = this.z / canvas.height;
+            let n = PerlinNoise.noise3(x, y, z);
+            this.context.lineTo(i, -(n * amplitude) + canvas.height);
+            this.context.stroke();
+        }
+    }
 
     draw() {
         this.context.clearRect(0, 0, canvas.width, canvas.height);
         this.context.save();
-        this.context.beginPath();
 
-        for (let i = 0; i < canvas.width; i += 10) {
-            let x = i / canvas.width;
-            let y = this.y / canvas.height;
-            let n = PerlinNoise.noise3(i / canvas.width,
-                this.y / canvas.height,
-                this.y / canvas.height);
-            this.context.lineTo(i, (n * 500) + 0);
-            this.context.stroke();
+        for (let i = 2; i < 10; i += 1) {
+            let color = 'green';
+            if (i % 2 == 0) {
+                color = 'red';
+            }
+            if (i % 3 == 0) {
+                color = 'orange';
+            }
+            this.context.strokeStyle = color;
+            this.oneline((i + 1) * 100);
         }
 
         this.y = mathf.sinNormalized(this.offset) * canvas.height;
         this.z = mathf.sinNormalized(this.offsetZ) * canvas.height;
-        this.offset += 0.01;
+        this.offset += 0.03;
         this.offsetZ += 0.02;
 
 
