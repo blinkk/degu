658576a5929d27caba14e31f132300c85724dd1c
Scott Yano <stmhawaii@gmail.com>
2019-06-26 17:31:41 +0900

    Add express app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'); //your original BE server
+const app = express();
+const config = require('./webpack/webpack.config.dev');
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware'); //webpack hot reloading middleware
+const compiler = webpack(config); //move your `devServer` config from `webpack.config.js`
+
+
+app.use(middleware(compiler, {
+    // webpack-dev-middleware options
+}));
+
+app.listen(8080, () => console.log('Run on port 3000'));
