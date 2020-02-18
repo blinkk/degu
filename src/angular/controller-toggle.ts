4abdd0b546edc5d615eb222dfc4da30d96c85322
Scott Yano <stmhawaii@gmail.com>
2020-02-18 19:33:14 +0900

    Add controller-toggle.

---
+++ src/angular/controller-toggle.ts
@@ -0,0 +1,57 @@
+
+
+/**
+ * A simple controller used to setup basic drawer like functionality.
+ *
+ * Example usage:
+ * ```ts
+ *
+ *  .toggle__drawer
+ *     display: none
+ * .toggle--open
+ *    .toggle__drawer
+ *        display: block
+ *
+ *
+ * <div class="toggle"
+ *  ng-class="{
+ *     'toggle--open':  toggleCtrl.expanded
+ *  }"
+ *  ng-controller="ToggleController as toggleCtrl">
+ *     <div class="toggle__content" ng-click="toggleCtrl.toggle()">
+ *         What city is the capital of Japan?
+ *     </div>
+ *     <div class="toggle__drawer">
+ *         Tokyo.
+ *     </div>
+ * </div>
+ * ```
+ *
+ *
+ */
+export class ToggleController {
+
+    private el: HTMLElement;
+    private expanded: boolean;
+
+    static get $inject() {
+        return ['$scope', '$element'];
+    }
+
+    constructor($element: ng.IAngularStatic) {
+        this.el = $element[0];
+        this.expanded = false;
+    }
+
+    public toggle():void {
+       this.expanded = !this.expanded;
+    }
+
+    public open():void {
+       this.expanded = true;
+    }
+
+    public close():void {
+       this.expanded = true;
+    }
+}
