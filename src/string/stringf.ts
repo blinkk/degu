b59b27c65eae0bee356b1404b0a37e0d234d5d43
Scott Yano <stmhawaii@gmail.com>
2020-06-17 06:40:06 +0900

    Add new time functions.

--- src/string/stringf.ts
+++ src/string/stringf.ts
@@ -20,4 +20,26 @@ export class stringf {
           .replace(/-+$/, '')
     }
 
+
+    /**
+     * A basic zero padding.
+     *
+     *
+     * ```
+     * stringf.zeroPad('1') ---> 01
+     * stringf.zeroPad('0') ---> 00
+     * stringf.zeroPad('9') ---> 09
+     * stringf.zeroPad('10') ---> 10
+     * stringf.zeroPad('22') ---> 10
+     * ```
+     *
+     * @param str
+     */
+    static zeroPad(str: string) {
+       if (str.toString().length == 1) {
+           str  = "0" + str;
+        }
+        return str;
+    }
+
 }
--- src/time/time.ts
+++ src/time/time.ts
@@ -1,4 +1,6 @@
 
+import { is } from '../is/is';
+
 /**
  * A class that helps with time.
  */
@@ -29,6 +31,84 @@ export class time {
     static timeDiffMs(startTime: number, endTime: number): number {
         return (endTime - startTime);
     }
+
+
+
+    /**
+     * Given a startTime and endTime, tests whether the provided testTime
+     * is within that time.
+     * @param startTime
+     * @param endTime
+     * @param testTime
+     */
+    static isInBetween(startTime: Date, endTime: Date, testTime: Date): boolean {
+        if (startTime && endTime) {
+            return testTime >= startTime && testTime < endTime;
+        } else if (startTime) {
+            return testTime >= startTime;
+        } else if (endTime) {
+            return testTime < endTime;
+        }
+        return false;
+    }
+
+
+    /**
+     * Converts a utc based time that you might created into the local time.
+     *
+     * ```
+     * const myUTCDate = new Date('2020-06-20 03:45:00');
+     * const theUTCDateInLocalTime = time.utcDateToLocalTimeZone(myUTCDate);
+     *
+     *
+     * // Do Stuff.  Now it is in local time zone.
+     * const diff = thisUTCDateInLocalTime - new Date();
+     * ```
+     * Now the theUTCDateInLocalTime is the UTC time adjusted to your
+     * local computer timezone.
+     *
+     * @param utcDate
+     */
+    static utcDateToLocalTimeZone(utcDate: Date): Date {
+        // Safari for some reason, defaults to utc and does the conversion
+        // for you.
+        if(is.safari()) {
+            return utcDate;
+        } else {
+            var timeOffsetInMS: number = utcDate.getTimezoneOffset() * 60000;
+            return new Date(utcDate.getTime() - timeOffsetInMS);
+        }
+    }
+
+
+    /**
+     * Use to calculate the countdown to a given time.  Note that the
+     * currentTime and endTime should both be in the same local timezone.
+     *
+     * Returns an object with days, hours, minutes, seconds.
+     *
+     * ```
+     * const now = new Date();
+     * const endTime = new Date('2021-06-20 03:34:00 UTC');
+     * const countDown = time.countdown(now, endTime);
+     *
+     *
+     * countdown.days; // the number of days
+     * countdown.hours; // the number of hours
+     * countdown.minutes; // the number of minutes
+     * countdown.seconds; // the number of seconds
+     * ```
+     */
+    static countdown(currentTime: Date, endTime: Date): Object {
+        var time = endTime.getTime() - currentTime.getTime();
+        return {
+            'days': Math.floor(time / (1000 * 60 * 60 * 24)),
+            'hours': Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            'minutes': Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+            'seconds': Math.floor((time % (1000 * 60)) / 1000)
+        }
+    }
+
 
 
 }
