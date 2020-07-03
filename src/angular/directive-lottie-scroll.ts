22128b5e5ec36a59a2152266947789f9cf49a350
Scott Yano <stmhawaii@gmail.com>
2020-07-03 14:34:39 +0900

    Fix documentation error in lottie directive.

--- src/angular/directive-lottie-scroll.ts
+++ src/angular/directive-lottie-scroll.ts
@@ -517,7 +517,7 @@ export class LottieController {
  *   import { LottieScrollEvents, LottieScrollEventPayload } from 'yano-js/lib/angular/directive-lottie-scroll';
  *
  *
- *   export default class PostHeroController {
+ *   export default class MyController {
  *     static get $inject() {
  *         return ['$scope', '$element'];
  *     }
