fe67dae16dbc79687a773687592257d6691a529d
Scott Yano <stmhawaii@gmail.com>
2020-07-06 14:25:26 +0900

    Add head fetch to pjax.

--- src/dom/pjax.ts
+++ src/dom/pjax.ts
@@ -16,10 +16,14 @@ export class pjax {
      *
      *    console.log(result['queryHtml']);  // The fetch result.  HTML as string.
      *    console.log(result['fullPageHtml']);  // The full page html of the fetch paged.
+     *    console.log(result['head']);  // The head content of the fetched page.
      *
      *
      *    // Inject the result into a page.
      *    document.querySelector('.mydiv').innerH:TML = result['queryHtml'];
+     *
+     *    // Update the head if you want.
+     *    document.querySelector('head').innerHTML = result['head'];
      *
      * })
      * ```
@@ -39,7 +43,8 @@ export class pjax {
             }).then((html:string)=> {
                 resolve({
                     queryHtml: pjax.queryHtmlString(html, querySelector),
-                    fullPageHtml: html
+                    fullPageHtml: html,
+                    head: pjax.getHead(html)
                 });
             }).catch((err)=> {
                 error(err);
@@ -55,6 +60,16 @@ export class pjax {
         let tmp = document.implementation.createHTMLDocument();
         tmp.body.innerHTML = html;
         return tmp.body.querySelector(targetElementQuerySelection).innerHTML;
+    }
+
+
+    /**
+     * Given an html string, returns the head.
+     */
+    static getHead(html:string) {
+        let tmp = document.implementation.createHTMLDocument();
+        tmp.documentElement.innerHTML = html;
+        return tmp.head.innerHTML;
     }
 
 
