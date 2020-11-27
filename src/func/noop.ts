5a2946c9cbdebfc993688b0e776f81f66d9ba50d
Angus MacDonald <amacdonald@google.com>
2020-11-26 21:12:41 -0600

    Add noop. Does nothing, but on purpose.

---
+++ src/func/noop.ts
@@ -0,0 +1,9 @@
+/**
+ * Does nothing. Use when you need to do nothing and want to make it clear that
+ * you fully understood you were doing nothing and really truly intended to do
+ * nothing.
+ *
+ * It does nothing, but on purpose and doesn't look like a bug or error.
+ * It exists solely for readability.
+ */
+export function noop(...args: any[]): any {}
