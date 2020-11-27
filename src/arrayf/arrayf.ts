02688fa94cfda7160985bcb54bbe0140e0608972
Angus MacDonald <amacdonald@google.com>
2020-11-27 12:45:31 -0600

    Add arrayf.split

--- src/arrayf/arrayf.ts
+++ src/arrayf/arrayf.ts
@@ -28,5 +28,38 @@ export class arrayf {
         return output;
     }
 
+    /**
+     * Splits an array by a given split value. Useful when casting to string
+     * for splitting won't cut it, such as when dealing with objects.
+     *
+     * ```
+     * const a = {};
+     * const b = {};
+     * const c = {};
+     * arrayf.split([a,b,c], b); // Returns [[a], [c]];
+     * arrayf.split([a,b,c,b,c], b); // Returns [[a], [c], [c]];
+     * arrayf.split([a,b,c,b,c], b, 1); // Returns [[a], [c, b, c]];
+     * ```
+     */
+    static split<T>(
+        values: T[],
+        splitValue: T,
+        limit: number = Number.POSITIVE_INFINITY
+    ): T[][] {
+        let remainder = values;
+        const result = [];
 
+        while (remainder.indexOf(splitValue) !== -1 && result.length < limit) {
+            const splitPosition: number = remainder.indexOf(splitValue);
+            const splitPiece: T[] = remainder.slice(0, splitPosition);
+            remainder = remainder.slice(splitPosition + 1);
+            result.push(splitPiece);
+        }
+
+        if (result.length < limit) {
+            result.push(remainder);
+        }
+
+        return result;
+    }
 }
