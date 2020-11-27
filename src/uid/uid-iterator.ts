b6fb7f33583772f3d44c0883f57d9eb395d3f07f
Angus MacDonald <amacdonald@google.com>
2020-11-26 21:59:40 -0600

    Fix lint errors in uid-iterator

--- src/uid/uid-iterator.ts
+++ src/uid/uid-iterator.ts
@@ -1,12 +1,12 @@
 export class UidIterator implements IterableIterator<number> {
-  private counter_: number = 0;
+  private counter: number = 0;
 
   next(value?: any): IteratorResult<number> {
-    this.counter_++;
+    this.counter++;
     return {
-      value: this.counter_,
-      done: this.counter_ >= Number.POSITIVE_INFINITY,
-    }
+      value: this.counter,
+      done: this.counter >= Number.POSITIVE_INFINITY
+    };
   }
 
   [Symbol.iterator](): UidIterator {
