a9f85b9ebf1331561554bdc41e2d6fb1b375f4bc
Angus MacDonald <amacdonald@google.com>
2020-11-27 12:29:49 -0600

    Add arrayf.wrapIndex

--- src/arrayf/arrayf.ts
+++ src/arrayf/arrayf.ts
@@ -28,5 +28,23 @@ export class arrayf {
         return output;
     }
 
-
+    /**
+     * Given an index that could be out-of-range for an array of the given
+     * length, it will return a valid index, as if the array values looped
+     * infinitely.
+     *
+     * ```
+     * wrapIndex(2, 7); // Returns 2
+     * wrapIndex(10, 7); // Returns 3
+     * wrapIndex(21, 7); // Returns 0
+     * wrapIndex(-1, 7); // Returns 6
+     * ```
+     */
+    static wrapIndex(index: number, length: number): number {
+        if (index < 0) {
+            return length + (index % length);
+        } else {
+            return index % length;
+        }
+    }
 }
