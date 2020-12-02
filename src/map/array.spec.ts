6be7c5c9702ab6ad2eed2a60f7b88d4f782a7a03
Angus MacDonald <amacdonald@google.com>
2020-12-02 17:38:51 -0600

    Add ArrayMap tests

---
+++ src/map/array.spec.ts
@@ -0,0 +1,14 @@
+import { ArrayMap } from './array';
+import test from 'ava';
+
+test('ArrayMap should always return an array', async t => {
+  const map = new ArrayMap();
+  t.true(map.get(false) instanceof Array);
+  t.true(map.get(1) instanceof Array);
+  t.true(map.get({}) instanceof Array);
+  t.true(map.get(0) instanceof Array);
+  t.true(map.get(null) instanceof Array);
+  t.true(map.get(-1) instanceof Array);
+  t.true(map.get([]) instanceof Array);
+  t.true(map.get('foo') instanceof Array);
+});
