186a79a54d080cf04b6a0e88033b7200f754408e
Angus MacDonald <amacdonald@google.com>
2020-12-14 18:10:14 -0600

    Add Dimensions2d class

---
+++ src/dom/root-element.ts
@@ -0,0 +1,2 @@
+export const ROOT_ELEMENT = document.children[0];
+// Can be cached on load since the browser isn't going to change at run time.
---
+++ src/dom/scroll-element.ts
@@ -0,0 +1,2 @@
+export const SCROLL_ELEMENT = document.scrollingElement || document.documentElement;
+// Can be cached on load since the browser isn't going to change at run time.
