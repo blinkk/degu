de16706c311e621033e62401db731395678cf1b3
Angus MacDonald <amacdonald@google.com>
2020-12-18 19:58:52 -0600

    Simplify visible-distance-from-root-service
    Remove sticky specific functionality.

--- src/dom/position/vertical/visible-distance-from-root-service.ts
+++ src/dom/position/vertical/visible-distance-from-root-service.ts
@@ -5,97 +5,12 @@
 import { DynamicDefaultMap } from '../../../map/dynamic-default';
 import { Scroll } from '../../scroll';
 import { Vector2dDom } from '../vector-2d-dom';
-import { dom, mathf, Raf } from '../../..';
+import { dom, Raf } from '../../..';
 
 const ignoredPositions = new Set(['fixed', 'absolute']);
 
-/**
- * Returns the offsetTop of the element if the scrolled sticky wasn't applied.
- */
-function getOffsetTopIgnoringSticky(element: HTMLElement): number {
-  const position = dom.getStyle(element).position;
-  // Short circuit if the given element isn't sticky
-  if (position !== 'sticky') {
-    return element.offsetTop;
-  } else {
-    /**
-     * Loop through all sibling and parent elements until the offset parent is
-     * found to find out where this element would be if it wasn't stickily
-     * positioned.
-     */
-    const previousSiblingHeight = getPreviousSiblingsHeight(element);
-    let previousParent = element.parentElement;
-    let parentPreviousSiblingHeight = 0;
-    while (previousParent !== element.offsetParent) {
-      parentPreviousSiblingHeight += getPreviousSiblingsHeight(previousParent);
-      previousParent = previousParent.parentElement;
-    }
-
-    return previousSiblingHeight + parentPreviousSiblingHeight;
-  }
-}
-
-/**
- * Returns the total offsetHeight of the element's previous siblings which are
- * relevant to calculating the offsetTop of the element as if it was not
- * sticky positioned.
- */
-function getPreviousSiblingsHeight(element: HTMLElement): number {
-  let previousSiblingHeight: number = 0;
-  let previousSibling: HTMLElement =
-      <HTMLElement>element.previousElementSibling;
-
-  /**
-   * Loop through previous siblings, totalling height, but ignoring fixed
-   * or absolute elements, and totalling only the height of stickied siblings.
-   */
-  while (previousSibling) {
-    const previousSiblingPosition = dom.getStyle(previousSibling).position;
-    if (!ignoredPositions.has(previousSiblingPosition)) {
-      if (previousSiblingPosition === 'sticky') {
-        previousSiblingHeight += previousSibling.offsetHeight;
-      } else {
-        previousSiblingHeight +=
-            previousSibling.offsetTop + previousSibling.offsetHeight;
-        break;
-      }
-    }
-    previousSibling = <HTMLElement>previousSibling.previousElementSibling;
-  }
-  return previousSiblingHeight;
-}
-
-/**
- * Returns how many pixels of scroll an element has been "stuck" for.
- */
-function getStuckDistance(element: HTMLElement): number {
-  const position = dom.getStyle(element).position;
-  if (position !== 'sticky') {
-    return 0;
-  } else {
-    const ignoringStickyOffsetTop = getOffsetTopIgnoringSticky(element);
-
-    const stickyContainer = element.parentElement;
-    const parentElementOffsetTop: number =
-        VisibleDistanceFromRootService.getSingleton()
-            .getVisibleDistanceFromRoot(stickyContainer);
-
-    const maxStickyDistance =
-        stickyContainer.offsetHeight - ignoringStickyOffsetTop -
-        element.offsetHeight;
-
-    const estimatedStickyDistance =
-        -1 * (ignoringStickyOffsetTop + parentElementOffsetTop);
-    return mathf.clamp(0, maxStickyDistance, estimatedStickyDistance);
-  }
-}
-
 function isFixed(element: HTMLElement) {
   return dom.getStyle(element).position === 'fixed';
-}
-
-function getIgnoreStickyOffset(candidateElement: HTMLElement): number {
-  return getTransformedOffset(candidateElement) - getStuckDistance(candidateElement);
 }
 
 function getTransformedOffset(candidateElement: HTMLElement): number {
@@ -139,7 +54,6 @@ export class VisibleDistanceFromRootService {
   private static singleton: VisibleDistanceFromRootService = null;
   private readonly raf: Raf;
   private readonly cache: DynamicDefaultMap<HTMLElement, number>;
-  private readonly cacheIgnoringSticky: DynamicDefaultMap<HTMLElement, number>;
 
   constructor() {
     this.raf = new Raf(() => this.loop());
@@ -147,11 +61,6 @@ export class VisibleDistanceFromRootService {
         DynamicDefaultMap.usingFunction(
             (element: HTMLElement) => {
               return getVisibleDistanceFromRoot(element, getTransformedOffset);
-            });
-    this.cacheIgnoringSticky =
-        DynamicDefaultMap.usingFunction(
-            (element: HTMLElement) => {
-              return getVisibleDistanceFromRoot(element, getIgnoreStickyOffset);
             });
     this.raf.start();
   }
@@ -173,6 +82,5 @@ export class VisibleDistanceFromRootService {
 
   private clearCaches() {
     this.cache.clear();
-    this.cacheIgnoringSticky.clear();
   }
 }
