00afecf181350d64ee27144cd58dd28ab94048c8
Scott Yano <stmhawaii@gmail.com>
2021-01-28 07:41:57 +0900

    Add scroll-to-on-focus Fixes #64

---
+++ src/dom/scroll-to-on-focus.ts
@@ -0,0 +1,186 @@
+
+import { DomWatcher } from './dom-watcher';
+import { dom } from './dom';
+
+
+export interface ScrollToOnFocusConfig {
+    /**
+     * The root parent element.
+     */
+    element: HTMLElement;
+
+    /**
+     * The top offset of the progress.
+     */
+    topProgressOffset: number,
+
+    /**
+     * The bottom offset of the progress.
+     */
+    bottomProgressOffset: number,
+
+
+    /**
+     * Whether to scroll to the element when its click on.
+     * Handy for debuggin.
+     */
+    mouseDown?: boolean
+
+    /**
+     * For any element with data-scroll-to-on-focus automatically set
+     * the tabindex=0.  For VO focus to be acquired, tabindex=0 is required
+     * so generally you can set this to true.
+     */
+    setTabIndex?: boolean
+}
+
+
+
+/**
+ * A class that jumps to a specific scroll point when a given
+ * element is focused on.
+ *
+ *
+ * This class is designed to help with a11y issues common with
+ * css-parallax, lottie-directive or any other module where
+ * scroll based progress is calculated.
+ *
+ *
+ * Consider the following example:
+ * ```
+ * <div sticky-300vh>
+ *   <div sticky-child>
+ *     <div chapter-1>
+ *     <div chapter-2>
+ *     <div chapter-3>
+ *     <div chapter-4>
+ *   </div>
+ * </div>
+ * ```
+ *
+ * Our module is 300vh tall and we have a sticky child.
+ * We then setup css-parallax and for every 0.25 (25%) progress,
+ * we hide the visibility / opacity of each chapter.
+ * In other words, as you scroll, the chapters fade in and out.
+ *
+ * The problem with this is that when using VO or another screenreader,
+ * the scroll position never updates as you focus on each chapter and
+ * creates a situation where you are selecting an element that might
+ * still have an opacity 0.
+ *
+ * To solve this, you can use this class.
+ *
+ * ```
+ * <div sticky-300vh>
+ *   <div sticky-child>
+ *     <div chapter-1 data-scroll-to-on-focus="0">
+ *     <div chapter-2 data-scroll-to-on-focus="0.25">
+ *     <div chapter-3 data-scroll-to-on-focus="0.5">
+ *     <div chapter-4 data-scroll-to-on-focus="0.75">
+ *   </div>
+ * </div>
+ *
+ * new ScrollToOnFocus({
+ *    element: document.querySelector('[sticky-child]'),
+ *    setTabIndex: true
+ *    // These values should match however you are calculating
+ *    // progress.
+ *    topProgressOffset: 0,
+ *    bottomProgressOffset: 0,
+ * })
+ *
+ * // The focus element require a tabindex so remove it if needed.
+ * [data-scroll-to-on-focus]:focus-visible
+ *   outline: 2px solid #1967D2
+ * [data-scroll-to-on-focus]:focus:not(:focus-visible)
+ *   outline: none
+ * ```
+ *
+ * Now when using VO, if you focus on chapter-2, it will jump to
+ * the progress 0.25 position.
+ *
+ *
+ */
+export class ScrollToOnFocus {
+    private element: HTMLElement;
+    private watcher: DomWatcher;
+    private selector: string;
+    private topProgressOffset: number;
+    private bottomProgressOffset: number;
+
+
+    constructor(private config: ScrollToOnFocusConfig) {
+
+        this.element = config.element;
+        this.watcher = new DomWatcher();
+        this.selector = 'data-scroll-to-on-focus';
+        this.topProgressOffset = config.topProgressOffset || 0;
+        this.bottomProgressOffset = config.bottomProgressOffset || 0;
+
+
+        const elements: Array<HTMLElement> =
+            Array.from(this.element.querySelectorAll(`[${this.selector}]`));
+
+        elements.forEach((el) => {
+
+            if(config.setTabIndex) {
+                el.tabIndex = 0;
+            }
+
+
+            this.watcher.add({
+                element: el,
+                on: 'focus',
+                callback: () => {
+                    this.handleFocus(el);
+                },
+                eventOptions: { capture: true }
+            });
+
+            if (config.mouseDown) {
+                this.watcher.add({
+                    element: el,
+                    on: 'mousedown',
+                    callback: () => {
+                        this.handleFocus(el);
+                    }
+                });
+            }
+        })
+    }
+
+
+    /**
+     * Handle focus on an element.
+     */
+    private handleFocus(focusedElement: HTMLElement) {
+        const targetPercent = focusedElement.getAttribute(this.selector);
+        if (targetPercent) {
+            this.scrollTo(+targetPercent);
+        }
+    }
+
+
+
+    /**
+     * Scroll to a specific window position
+     * @param targetPercent
+     */
+    private scrollTo(targetPercent: number) {
+        // Given the top and bottom progress offset, get the windowY value of
+        // the provided percent.
+        const scrollY = dom.getScrollYAtPercent(
+            this.element,
+            this.topProgressOffset, this.bottomProgressOffset,
+            targetPercent
+        );
+
+        // Scroll To that point.
+        window.scrollTo(0, scrollY);
+    }
+
+    public dispose() {
+        this.watcher && this.watcher.dispose();
+    }
+
+}
