dd08c6f6b8d5f9c4f2d3a32e1949de1501c20fc6
Angus MacDonald <amacdonald@google.com>
2021-01-29 14:54:38 -0600

    Comment and improve TrackedListener

--- src/dom/tracked-listener.ts
+++ src/dom/tracked-listener.ts
@@ -8,6 +8,12 @@ type Listener = [HTMLElement, string, EventListenerOrEventListenerObject];
  * dynamically added listeners.
  */
 export class TrackedListener {
+  /**
+   * Adds an event listener that can be tracked via the returned ID.
+   * @param element
+   * @param event
+   * @param callback
+   */
   static add(
       element: HTMLElement,
       event: string,
@@ -19,18 +25,30 @@ export class TrackedListener {
     return id;
   }
 
+  /**
+   * Removes the event listener tracked via the given ID.
+   */
   static remove(id: number) {
     const listener = this.listeners.get(id);
     listener[0].removeEventListener(listener[1], listener[2]);
     this.listeners.delete(id);
   }
 
+  /**
+   * Get the next available ID.
+   *
+   * If the ID starts to exceed the safe range then it will restart at the
+   * minimum safe ID.
+   */
   static getNextId(): number {
-    const id = TrackedListener.uid;
-    if (TrackedListener.uid === Number.MAX_SAFE_INTEGER) {
-      TrackedListener.uid = Number.MIN_SAFE_INTEGER;
-    } else {
-      TrackedListener.uid++;
+    let id = TrackedListener.uid;
+    while (this.listeners.has(id)) {
+      if (TrackedListener.uid === Number.MAX_SAFE_INTEGER) {
+        TrackedListener.uid = Number.MIN_SAFE_INTEGER;
+      } else {
+        TrackedListener.uid++;
+      }
+      id = TrackedListener.uid;
     }
     return id;
   }
