9bc575344d97d94a947207aaa91236bbf2acb9c5
Angus MacDonald <amacdonald@google.com>
2021-01-29 20:41:56 -0600

    Remove arrayf.filterUntilFalse

--- src/arrayf/arrayf.ts
+++ src/arrayf/arrayf.ts
@@ -116,20 +116,4 @@ export class arrayf {
         }
         return result;
     }
-
-    /**
-     * Return the given values in order up until the value that returns a
-     * false value when passed to the given condition function.
-     * @param values
-     * @param conditionFn
-     */
-    static filterUntilFalse<T>(
-        values: T[], conditionFn: (value: T, index: number) => boolean
-    ): T[] {
-        let index: number = 0;
-        while (index < values.length && conditionFn(values[index], index)) {
-            index++;
-        }
-        return values.slice(0, index);
-    }
 }
