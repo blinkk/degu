40e6c9ab0e19135672620175e2e68644d283c765
Angus MacDonald <amacdonald@google.com>
2021-02-17 15:32:32 -0600

    Add arrayf tests

--- src/arrayf/arrayf.spec.ts
+++ src/arrayf/arrayf.spec.ts
@@ -15,3 +15,29 @@ test('max', t => {
   t.is(arrayf.max(values, c, b), valY);
   t.is(arrayf.max(values, c, a), valX);
 });
+
+test('min', t => {
+  type Val = {a: number, b: number, c: number};
+  const valX = { a: 2, b: 3, c: 7 };
+  const valY = { a: 1, b: 4, c: 7 };
+  const values = [valX, valY];
+  const a = (value: Val) => value.a;
+  const b = (value: Val) => value.b;
+  const c = (value: Val) => value.c;
+  t.is(arrayf.min(values, a), valY);
+  t.is(arrayf.min(values, b), valX);
+  t.is(arrayf.min(values, c), valX);
+  t.is(arrayf.min(values, c, b), valX);
+  t.is(arrayf.min(values, c, a), valY);
+});
+
+test('loopSlice', t => {
+  const values = [1, 2, 3, 4, 5, 6, 7, 8];
+  t.deepEqual(arrayf.loopSlice(values, 0, 1), [1]);
+  t.deepEqual(arrayf.loopSlice(values, 0, 1, 1), [1]);
+  t.deepEqual(arrayf.loopSlice(values, 0, 1, -1), [1, 8, 7, 6, 5, 4, 3]);
+  t.deepEqual(arrayf.loopSlice(values, 4, 1, -1), [5, 4, 3]);
+  t.deepEqual(arrayf.loopSlice(values, 4, 6, -1), [5, 4, 3, 2, 1, 8]);
+  t.deepEqual(arrayf.loopSlice(values, 4, 6), [5, 6]);
+  t.deepEqual(arrayf.loopSlice(values, 4, 7, 1), [5, 6, 7]);
+});
