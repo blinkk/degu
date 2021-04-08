973db1a0f4452803931fd26d5ea9833bdc0fe2d9
Scott Yano <stmhawaii@gmail.com>
2021-04-09 06:42:00 +0900

    Add PreventScroll.

---
+++ src/dom/prevent-scroll.ts
@@ -0,0 +1,67 @@
+
+import { DomWatcher } from './dom-watcher';
+
+
+/**
+ * A class that prevents the user from scrolling.
+ *
+ * This class can be useful in "Freezing" the screen to the current
+ * scroll position.
+ *
+ * ```
+ * const p = new PreventScroll();
+ *
+ * // No scrollling allowed
+ * p.enableScroll(false);
+ *
+ * // All scrolling allowed
+ * p.enableScroll(true);
+ *
+ * // Disposes and reenables scroll.
+ * p.dispose();
+ * ```
+ */
+export class PreventScroll {
+
+    private domWatcher: DomWatcher;
+    constructor() {
+        this.domWatcher = new DomWatcher();
+
+    }
+
+
+    private preventDefault(e:any) {
+        e.preventDefault();
+    }
+
+
+    public enableScroll(enable: boolean = true) {
+        if(enable) {
+          this.domWatcher.removeAll();
+        } else {
+
+            const eventTypes = [
+                'DomMouseScroll',
+                'Scroll',
+                'wheel',
+                'mouseWheel',
+                'keydown',
+                'touchmove',
+            ]
+            eventTypes.forEach((type)=> {
+                this.domWatcher.add({
+                    // @ts-ignore
+                    element: document,
+                    on: type,
+                    eventOptions: { passive: false },
+                    callback: this.preventDefault.bind(this)
+                });
+            })
+        }
+    }
+
+
+    public dispose() {
+        this.domWatcher.dispose();
+    }
+}
