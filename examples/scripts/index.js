4844f050e8635e89626832bc9d3586a67dd183fd
Scott Yano <stmhawaii@gmail.com>
2021-11-30 07:32:53 +0900

    feat: Add video component

--- examples/scripts/index.js
+++ examples/scripts/index.js
@@ -110,7 +110,7 @@ import WebGlImageSequenceSample9 from '../webgl-image-sequence9';
 import X2Sample from '../x2';
 import XSample from '../x';
 import InviewSample from '../inview';
-import DeguImageSample from '../degu-image';
+import LitSample from '../lit';
 
 
 
@@ -223,7 +223,7 @@ const samples = {
   'glslPlayground29': GlslPlayground29,
   'glslPlayground30': GlslPlayground30,
   'inviewSample': InviewSample,
-  'deguImageSample': DeguImageSample,
+  'litSample': LitSample,
 };
 
 class Main {
---
+++ src/lit/video.test.ts
@@ -0,0 +1,83 @@
+import {assert, expect, fixture, html, aTimeout} from '@open-wc/testing';
+import {DeguVideo} from './video';
+import * as dom from '../dom/dom';
+import * as func from '../func/func';
+
+// Define it.
+!window.customElements.get('degu-video') &&
+  window.customElements.define('degu-video', DeguVideo);
+
+const addStylesToPage = func.runOnlyOnce(() => {
+  dom.addStylesToPage(`
+    degu-video {
+      display: inline-block;
+      line-height: 0;
+      width: 100%;
+    }
+    video {
+        width: 100%;
+    }
+`);
+});
+
+describe('Mqn3Video', () => {
+  let root: HTMLElement;
+  let deguVideo: DeguVideo;
+  let video: HTMLVideoElement;
+
+  beforeEach(async () => {
+    addStylesToPage();
+    root = await fixture(
+      html`<div id="root" style="width: 500px">
+        <degu-video
+          src="https://storage.googleapis.com/googwebreview.appspot.com/grow-ext-file-upload/1638127354399989/PXL_20211127_042647380.mp4"
+          width="640"
+          height="640"
+          style="aspect-ratio: 1"
+          class=""
+        ></degu-video>
+      </div>`
+    );
+
+    deguVideo = root.querySelector('degu-video');
+    video = root.querySelector('video');
+  });
+
+  it('is defined', () => {
+    assert.instanceOf(deguVideo, DeguVideo);
+    assert.instanceOf(video, HTMLVideoElement);
+  });
+
+  it('renders an video with the correct attributes', async () => {
+    expect(video.hasAttribute('disableRemotePlayback')).to.equal(true);
+    expect(video.hasAttribute('muted')).to.equal(true);
+    expect(video.hasAttribute('playsinline')).to.equal(true);
+  });
+
+  it('hides the inner video if aria-label is set', async () => {
+    root = await fixture(
+      html`<div id="root" style="width: 500px">
+        <degu-video
+          src="https://storage.googleapis.com/googwebreview.appspot.com/grow-ext-file-upload/1638127354399989/PXL_20211127_042647380.mp4"
+          aria-label="hello"
+          width="640"
+          height="640"
+          style="aspect-ratio: 1"
+          class=""
+        ></degu-video>
+      </div>`
+    );
+
+    deguVideo = root.querySelector('degu-video');
+    video = root.querySelector('video');
+
+    expect(deguVideo.getAttribute('aria-label')).to.equal('hello');
+    expect(deguVideo.getAttribute('role')).to.equal('img');
+    expect(video.hasAttribute('aria-hidden')).to.equal(true);
+  });
+
+  it('should not play the video by default', async () => {
+    await aTimeout(10);
+    expect(dom.testVideoIsPlaying(video)).to.equal(false);
+  });
+});
