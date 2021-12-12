38d8edcf14927172b1f1089eb1b9714f1c0f9423
Scott Yano <stmhawaii@gmail.com>
2021-12-13 03:27:49 +0900

    feat: Add Swipe

--- examples/scripts/index.js
+++ examples/scripts/index.js
@@ -111,6 +111,7 @@ import X2Sample from '../x2';
 import XSample from '../x';
 import InviewSample from '../inview';
 import ComponentsSample from '../components';
+import UiSwipeSample from '../ui-swipe';
 
 
 
@@ -224,6 +225,7 @@ const samples = {
   'glslPlayground30': GlslPlayground30,
   'inviewSample': InviewSample,
   'ComponentsSample': ComponentsSample,
+  'UiSwipeSample': UiSwipeSample,
 };
 
 class Main {
---
+++ src/ui/swipe.ts
@@ -0,0 +1,172 @@
+import {DomWatcher} from '../dom/dom-watcher';
+import {EventCallback, EventManager, EventDispatcher} from './events';
+import * as dom from '../dom/dom';
+
+export enum SwipeEvent {
+  UP = 'swipeUp',
+  DOWN = 'swipeDown',
+  LEFT = 'swipeLeft',
+  RIGHT = 'swipeRight',
+}
+
+/**
+ * A basic class that allows you to watch directional swipe events on a given
+ * element.
+ *
+ *
+ * Usage:
+ * ```ts
+ * const swiper = new Swipe(myElement);
+ *
+ * swiper.on(Swipe.Events.UP, ()=> {
+ *   // swipe up.
+ * })
+ * swiper.on(Swipe.Events.LEFT, ()=> {
+ *   // swipe left
+ * })
+ *
+ * // Later
+ * swiper.dispose();
+ * ```
+ *
+ *
+ *
+ * Events are also fired on the element itself.
+ *
+ * ```ts
+ * const swiper = new Swipe(myElement);
+ * myElement.addEventListener(Swipe.Events.UP, ()=> {
+ *   // swipe up.
+ * })
+ * ```
+ */
+export class Swipe implements EventDispatcher {
+  private rootElement: HTMLElement;
+  private eventManager: EventManager;
+  private watcher: DomWatcher;
+  private x: number;
+  private y: number;
+  private allowSwipe = false;
+
+  constructor(rootElement: HTMLElement) {
+    this.rootElement = rootElement;
+    this.eventManager = new EventManager();
+    this.watcher = new DomWatcher();
+
+    const isTouchSupported = 'ontouchstart' in document.documentElement;
+
+    // Add listeners
+    if (isTouchSupported) {
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'touchstart',
+        eventOptions: {passive: true},
+        callback: this.onPointerDown.bind(this),
+      });
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'touchend',
+        eventOptions: {passive: true},
+        callback: this.onPointerUp.bind(this),
+      });
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'touchmove',
+        eventOptions: {passive: true},
+        callback: this.onPointerMove.bind(this),
+      });
+    } else {
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'mousedown',
+        eventOptions: {passive: true},
+        callback: this.onPointerDown.bind(this),
+      });
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'mouseup',
+        eventOptions: {passive: true},
+        callback: this.onPointerUp.bind(this),
+      });
+      this.watcher.add({
+        element: this.rootElement as HTMLElement,
+        on: 'mousemove',
+        eventOptions: {passive: true},
+        callback: this.onPointerMove.bind(this),
+      });
+    }
+  }
+
+  static Events = SwipeEvent;
+
+  private onPointerDown(e: TouchEvent | MouseEvent) {
+    if (e instanceof TouchEvent) {
+      this.x = e.touches[0].clientX;
+      this.y = e.touches[0].clientY;
+    }
+    if (e instanceof MouseEvent) {
+      this.x = e.x;
+      this.y = e.y;
+    }
+    this.allowSwipe = true;
+  }
+
+  private onPointerUp() {
+    this.allowSwipe = false;
+  }
+
+  private onPointerMove(e: TouchEvent) {
+    if (!this.x || !this.y || !this.allowSwipe) {
+      return;
+    }
+
+    let currentX;
+    let currentY;
+    if (e instanceof TouchEvent) {
+      currentX = e.touches[0].clientX;
+      currentY = e.touches[0].clientX;
+    }
+
+    if (e instanceof MouseEvent) {
+      currentX = e.x;
+      currentY = e.y;
+    }
+
+    const diffX = currentX - this.x;
+    const diffY = currentY - this.y;
+
+    // Determine swipe direction
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      if (diffX > 0) {
+        this.eventManager.dispatch(Swipe.Events.LEFT);
+        dom.event(this.rootElement, Swipe.Events.LEFT, {});
+      } else {
+        this.eventManager.dispatch(Swipe.Events.RIGHT);
+        dom.event(this.rootElement, Swipe.Events.RIGHT, {});
+      }
+    } else {
+      if (diffY > 0) {
+        this.eventManager.dispatch(Swipe.Events.UP);
+        dom.event(this.rootElement, Swipe.Events.UP, {});
+      } else {
+        this.eventManager.dispatch(Swipe.Events.DOWN);
+        dom.event(this.rootElement, Swipe.Events.DOWN, {});
+      }
+    }
+
+    this.allowSwipe = false;
+  }
+
+  on(eventName: string, callback: Function) {
+    this.eventManager.on(eventName, callback as EventCallback);
+  }
+
+  off(eventName: string, callback: Function) {
+    this.eventManager.off(eventName, callback as EventCallback);
+  }
+
+  dispose() {
+    this.eventManager.dispose();
+    this.watcher.dispose();
+  }
+}
