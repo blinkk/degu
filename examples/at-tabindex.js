a2f6c9c14bc4cbc7a84485104b7ac6762ce74453
Scott Yano <stmhawaii@gmail.com>
2022-03-16 08:38:28 +0900

    feat: Add tab-index

---
+++ examples/at-tabindex.js
@@ -0,0 +1,18 @@
+
+import {AtTabIndex} from '../lib/dom/at-tabindex';
+
+export default class AtTabIndexSample {
+  constructor() {
+    let root = document.getElementById('root');
+
+    const atClass = new AtTabIndex({
+      element: root,
+      conditions: [
+        ['desktop', ()=> window.innerWidth > 1000],
+        ['tablet', ()=> window.innerWidth < 1000 && window.innerWidth > 780],
+        ['mobile', ()=> window.innerWidth < 780],
+      ],
+      watchResize: true
+    })
+  }
+}
